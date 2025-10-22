c9030a77fcb31c91416ef3cad066840dd5fe3394
belmo <belmokhtarpatrick@gmail.com>
2025-10-22 23:04:16 +0200

    feat: add login and sign up endpoints

--- Pariqas.Api/Controllers/AuthenticationController.cs
+++ Pariqas.Api/Controllers/AuthenticationController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 using Pariqas.Api.Services;
+using Pariqas.Http.Requests;
+using Pariqas.Http.Responses;
+using Pariqas.Models.Identity;
 
 namespace Pariqas.Api.Controllers;
 
@@ -9,9 +13,56 @@ namespace Pariqas.Api.Controllers;
 public sealed class AuthenticationController : ControllerBase
 {
     private readonly JwtService _jwt;
+    private readonly UserManager<User> _userManager;
     
-    public AuthenticationController(JwtService jwt)
+    public AuthenticationController(JwtService jwt, UserManager<User>  userManager)
     {
         _jwt = jwt;
+        _userManager = userManager;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<TokenResponse>> Login([FromBody] LoginRequest request)
+    {
+        var user = await _userManager.FindByEmailAsync(request.Email);
+        if (user is null)
+        {
+            return NotFound();
+        }
+        
+        bool valid = await _userManager.CheckPasswordAsync(user, request.Password);
+        if (!valid)
+        {
+            return Unauthorized();
+        }
+
+        string token = _jwt.GenerateToken(user);
+
+        return Ok(new TokenResponse(token));
+    }
+
+    [HttpPost("sign-up")]
+    public async Task<ActionResult> SignUp([FromBody] SignUpRequest request)
+    {
+        bool exists = await _userManager.FindByEmailAsync(request.Email) is not null;
+        if (exists)
+        {
+            return Conflict();
+        }
+
+        var user = new User
+        {
+            UserName = request.Email,
+            Email = request.Email,
+            CreatedAt = DateTime.UtcNow,
+        };
+        
+        var result = await _userManager.CreateAsync(user, request.Password);
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors);
+        }
+
+        return Ok();
     }
 }
