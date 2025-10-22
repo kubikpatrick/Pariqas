549e22cb922427b973207ae74e7e99d526565cca
belmo <belmokhtarpatrick@gmail.com>
2025-10-22 23:03:59 +0200

    feat: add ASP.NET Core extensions

--- Pariqas.Api/Extensions/AspNetCoreExtensions.cs
+++ Pariqas.Api/Extensions/AspNetCoreExtensions.cs
@@ -1,6 +1,64 @@
-﻿namespace Pariqas.Api.Extensions;
+﻿using System.Text;
 
-public static class AspNetCoreExtensions
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+
+using Pariqas.Api.Data;
+using Pariqas.Api.Services;
+using Pariqas.Models.Identity;
+
+namespace Pariqas.Api.Extensions;
+
+public static class DependencyInjectionExtensions
 {
-    
+    public static IServiceCollection AddIdentity(this IServiceCollection services)
+    {
+        services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddTokenAuthentication(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<JwtService>();
+        services.AddAuthentication(options => 
+        { 
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                ValidateIssuer = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = configuration["Jwt:Issuer"],
+                ValidAudience = configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"])),
+            };
+            
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    var token = context.Request.Query[Constants.AccessToken];
+                    var path = context.HttpContext.Request.Path;
+                    
+                    if (!string.IsNullOrEmpty(token) && path.StartsWithSegments("/hubs"))
+                    {
+                        context.Token = token;
+                    }
+                    
+                    return Task.CompletedTask;
+                },
+                
+                OnAuthenticationFailed = context => Task.CompletedTask,
+            };
+        });
+        
+        return services;
+    }
 }
