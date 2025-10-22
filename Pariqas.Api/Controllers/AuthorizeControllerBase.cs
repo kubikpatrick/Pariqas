d350e7ee65d49e12ef5904650848b44c51378c17
patrick <patrick.kubik@outlook.com>
2025-10-22 23:28:13 +0200

    refactor: simplify CurrentUserId implementation in AuthorizeControllerBase

--- Pariqas.Api/Controllers/AuthorizeControllerBase.cs
+++ Pariqas.Api/Controllers/AuthorizeControllerBase.cs
@@ -2,17 +2,11 @@
 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.JsonWebTokens;
 
 namespace Pariqas.Api.Controllers;
 
 [Authorize]
 public abstract class AuthorizeControllerBase : ControllerBase
 {
-    protected AuthorizeControllerBase()
-    {
-        CurrentUserId = User.FindFirstValue(JwtRegisteredClaimNames.Sub)!;
-    }
-
-    protected readonly string CurrentUserId;
+    protected string CurrentUserId => User.FindFirstValue(ClaimTypes.NameIdentifier)!;
 }
