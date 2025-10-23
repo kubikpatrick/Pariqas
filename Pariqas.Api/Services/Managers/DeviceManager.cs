280273aa9bb5e4c9142cce0effb5d3157867450f
belmo <belmokhtarpatrick@gmail.com>
2025-10-23 12:42:01 +0200

    feat: implement result pattern usage and manager's methods (delete, update, get)

--- Pariqas.Api/Services/Managers/DeviceManager.cs
+++ Pariqas.Api/Services/Managers/DeviceManager.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Caching.Memory;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 using Pariqas.Api.Data;
+using Pariqas.Api.Executors;
+using Pariqas.Models;
 using Pariqas.Models.Devices;
 
 namespace Pariqas.Api.Services.Managers;
@@ -17,32 +20,74 @@ public sealed class DeviceManager
         _cache = cache;
     }
 
-    public async Task<Device?> FindByIdAsync(string deviceId)
+    public async Task<Device?> FindOwnedByIdAsync(string deviceId, string userId)
     {
         if (_cache.TryGetValue(deviceId, out Device? device))
         {
-            return device;
+            return device.UserId == userId ? device : null;
         }
 
         device = await _context.Devices.FindAsync(deviceId);
-        if (device is not null)
+        if (device is not null && device.UserId == userId)
         {
             _cache.Set(deviceId, device, TimeSpan.FromMinutes(15));
+            
+            return device;
         }
 
-        return device;
+        return null;
+    }
+
+    public async Task<List<Device>> FindAllForUserAsync(string userId)
+    {
+        return await _context.Devices.Where(d => d.UserId == userId).ToListAsync();
     }
     
+    public async Task<OperationResult> CreateAsync(Device device)
+    {
+        if (string.IsNullOrEmpty(device.Name))
+        {
+            return OperationResult.Fail("Device name cannot be empty.");
+        }
 
-    public async Task DeleteAsync(Device device)
+        return await ResultExecutor.ExecuteAsync(async () =>
+        {
+            await _context.Devices.AddAsync(device);
+            await _context.SaveChangesAsync();
+        
+            _cache.Set(device.Id, device, TimeSpan.FromMinutes(15));
+        });
+    }
+    
+    public async Task<OperationResult> DeleteAsync(Device device)
     {
         if (_cache.TryGetValue(device.Id, out _))
         {
             _cache.Remove(device.Id);
         }
-
+        
         _context.Devices.Remove(device);
+        
+        return await ResultExecutor.ExecuteAsync(async () =>
+        {
+            await _context.SaveChangesAsync();
+        });
+    }
+    
+    public async Task<OperationResult> UpdateLocationAsync(Device device, Location location)
+    {
+        if (device.Location.Longitude == location.Longitude && device.Location.Latitude == location.Latitude)
+        {
+            return OperationResult.Success();
+        }
+        
+        device.Location = location;
 
-        await _context.SaveChangesAsync();
+        _context.Devices.Update(device);
+        
+        return await ResultExecutor.ExecuteAsync(async () =>
+        {
+            await _context.SaveChangesAsync();
+        });
     }
 }
