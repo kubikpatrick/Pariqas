9ac64514f0607da21258d48cc7b5b5a84da45a8e
patrick <patrick.kubik@outlook.com>
2025-11-14 22:43:54 +0100

    feat: add HealthController with basic health check endpoint

---
+++ Pariqas.Api/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pariqas.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public sealed class HealthController : ControllerBase
+{
+    public HealthController()
+    {
+        
+    }
+    
+    [HttpGet]
+    public ActionResult Index() => Ok();
+}
