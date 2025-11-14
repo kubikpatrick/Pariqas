78a4678e40a3b25d7634525d2dfcaed62a176959
patrick <patrick.kubik@outlook.com>
2025-11-14 18:44:23 +0100

    feat: add LocationHub for managing SignalR connections

---
+++ Pariqas.Api/Hubs/LocationHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Pariqas.Api.Hubs;
+
+[Authorize]
+public sealed class LocationHub : Hub
+{
+    public LocationHub()
+    {
+        
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        return base.OnConnectedAsync();
+    }
+}
