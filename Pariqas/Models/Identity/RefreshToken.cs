c9a421bf3ec902c35bce1eb29b319ced3ab31046
patrick <patrick.kubik@outlook.com>
2025-11-14 18:40:09 +0100

    feat: add RefreshToken model with properties and relationships

---
+++ Pariqas/Models/Identity/RefreshToken.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Pariqas.Models.Identity;
+
+[PrimaryKey(nameof(Id))]
+public sealed class RefreshToken
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Required]
+    public string Id { get; set; }
+    
+    [Required]
+    public string Token { get; set; }
+    
+    [Required]
+    public DateTime ExpiresAt { get; set; }
+    
+    [Required]
+    public bool IsRevoked { get; set; }
+    
+    [Required]
+    public string UserId { get; set; }
+    
+    [ForeignKey(nameof(UserId))]
+    public User User { get; set; }
+}
