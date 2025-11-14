1a38756c4012e0872d50d61d4dd055694a285abd
patrick <patrick.kubik@outlook.com>
2025-11-14 18:46:16 +0100

    feat: add default file

---
+++ Pariqas.Web/Program.cs
@@ -0,0 +1,29 @@
+using Blazored.LocalStorage;
+
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using Pariqas.Web.Extensions;
+using Pariqas.Web.Services;
+
+namespace Pariqas.Web;
+
+public sealed class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddAuthorizationCore();
+        builder.Services.AddBlazoredLocalStorage();
+        builder.Services.AddHttpClient();
+        builder.Services.AddScoped<TokenService>();
+        builder.Services.AddScoped<AuthenticationStateProvider, JwtAuthenticationStateProvider>();
+
+        await builder.Build().RunAsync();
+    }
+}
