03230ac5771c11b9792bd2fdc18a6608053ea7f9
patrick <patrick.kubik@outlook.com>
2025-11-17 00:03:47 +0100

    feat: implement ServerUrlService with local storage support

---
+++ Pariqas.Web/Services/ServerUrlService.cs
@@ -0,0 +1,28 @@
+﻿using Blazored.LocalStorage;
+
+namespace Pariqas.Web.Services;
+
+public sealed class ServerUrlService
+{
+    private readonly ILocalStorageService _localStorage;
+    
+    public ServerUrlService(ILocalStorageService localStorage)
+    {
+        _localStorage = localStorage;
+    }
+    
+    public async Task<string?> GetServerUrlAsync()
+    {
+        return await _localStorage.GetItemAsStringAsync(Constants.ServerUrl);
+    }
+
+    public async Task SetServerUrlAsync(string url)
+    {
+        await _localStorage.SetItemAsStringAsync(Constants.ServerUrl, url);
+    }
+    
+    public async Task RemoveServerUrlAsync()
+    {
+        await _localStorage.RemoveItemAsync(Constants.ServerUrl);
+    }
+}
