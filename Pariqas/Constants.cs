ef55fb6172b5fdc1eeba4a94664ad6fa066fb201
patrick <patrick.kubik@outlook.com>
2025-11-17 00:04:21 +0100

    feat: add RefreshToken and ServerUrl constants

--- Pariqas/Constants.cs
+++ Pariqas/Constants.cs
@@ -3,4 +3,6 @@ namespace Pariqas;
 public readonly struct Constants
 {
     public const string AccessToken = "ACCESS_TOKEN";
+    public const string RefreshToken = "REFRESH_TOKEN";
+    public const string ServerUrl = "SERVER_URL";
 }
